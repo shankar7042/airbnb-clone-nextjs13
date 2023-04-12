b8ff85a240f2cd04df7351f051ce753b5d949283
shankar7042 <shankar.kumar.4171@gmail.com>
2023-04-13 02:56:30 +0530

    adjusted top margin of modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -71,7 +71,7 @@ const Modal: React.FC<ModalProps> = ({
         <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
           {/* CONTENT */}
           <div
-            className={`transform duration-300 h-full 
+            className={`transform duration-300 h-full mt-36
             ${showModal ? "translate-y-0" : "translate-y-full"}
             ${showModal ? "opactiy-100" : "opacity-0"}
           `}
